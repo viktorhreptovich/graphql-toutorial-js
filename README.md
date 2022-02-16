ff937685116c4a3cf5bb6689cc2cff7f813bbbed
Viktor Khreptovich <vhreptovich@gmail.com>
2022-02-16 21:28:59 +0300

    Init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# graphql-toutorial-js
+# GraphQL tutorial
+
+https://www.youtube.com/watch?v=kZs7CXrtT-s&list=PLNkWIWHIRwMF2sVLwzRef0Cu5kzAOeRcu&index=1
---
+++ server/server/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+const schema = require('../schema/schema');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3005;
+
+mongoose.connect('<connection string>', { useNewUrlParser: true})
+
+app.use(cors());
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true,
+}));
+
+const dbConnection = mongoose.connection;
+dbConnection.on('error', err => console.log(`Connection error: ${err}`));
+dbConnection.once('open', () => console.log('Connected to DB!'));
+
+app.listen(PORT, err => {
+  err ? console.log(err) : console.log('Server started!');
+});
